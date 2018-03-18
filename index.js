4768e3c7cc28cbea9039726033996fdfdf00cfa1
Subhajit Sahu <wolfram77@gmail.com>
2018-03-18 23:24:26 +0530

    :snowman: log request details

--- index.js
+++ index.js
@@ -44,6 +44,15 @@ server.on('listening', () => {
 
 X.use(bodyParser.json());
 X.use(bodyParser.urlencoded({'extended': true}));
+X.use((req, res, next) => {
+  console.log();
+  var {port, family, address} = req.socket.address();
+  var from = req.headers['x-forwarded-for']||`${address}:${port} (${family})`;
+  var proto = req.headers['x-forwarded-proto']||'http';
+  var length = req.headers['content-length']||'?';
+  console.log(`${from} -> ${proto} ${req.method} ${req.url} length:${length}`);
+  next();
+});
 X.all('/bot', (req, res) => {
   console.log('req.headers', req.headers);
   console.log('req.params', req.params);
