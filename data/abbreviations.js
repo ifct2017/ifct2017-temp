1cf030ed457cf7d88a2d081141f29e08d19febd9
Subhajit Sahu <wolfram77@gmail.com>
2018-03-24 04:11:26 +0530

    :banana: enable abbreviations table

--- data/abbreviations.js
+++ data/abbreviations.js
@@ -3,32 +3,31 @@ const csv = require('csv');
 const fs = require('fs');
 
 const NAME = new Map();
-const TSVECTOR = `setweight(to_tsvector('english', "abbr"), 'A')||`+
-  `setweight(to_tsvector('english', "full"), 'B')||`;
+const TSVECTOR = `setweight(to_tsvector('english', "code"), 'A')||`+
+  `setweight(to_tsvector('english', "name"), 'B')`;
 
 function createTable(db) {
   var z = `CREATE TABLE IF NOT EXISTS "abbreviations" (`+
     ` "code" TEXT NOT NULL,`+
     ` "name" TEXT NOT NULL,`+
-    ` "tags" TEXT NOT NULL,`+
     ` PRIMARY KEY ("code")`+
     `);\n`+
-    `CREATE OR REPLACE VIEW "columns_tsvector" AS `+
-    ` SELECT *, ${TSVECTOR} AS "tsvector" FROM "columns";\n`;
+    `CREATE OR REPLACE VIEW "abbreviations_tsvector" AS `+
+    ` SELECT *, ${TSVECTOR} AS "tsvector" FROM "abbreviations";\n`;
   return db.query(z);
 };
 
 function createIndex(db) {
-  var z = `CREATE INDEX IF NOT EXISTS "columns_name_idx" ON "columns" ("name");\n`;
-  z += `CREATE INDEX IF NOT EXISTS "columns_tsvector_idx" ON "columns" USING GIN ((${TSVECTOR}));\n`;
+  var z = `CREATE INDEX IF NOT EXISTS "abbreviations_name_idx" ON "abbreviations" ("name");\n`;
+  z += `CREATE INDEX IF NOT EXISTS "abbreviations_tsvector_idx" ON "abbreviations" USING GIN ((${TSVECTOR}));\n`;
   return db.query(z);
 };
 
-function insert(db, cols) {
-  var z = `INSERT INTO "columns" ("code", "name", "tags") VALUES\n`;
-  for(var col of cols) {
-    z += `('${col.code}', '${col.name}', '${col.tags}'),\n`;
-    NAME.set(col.code, col.name);
+function insert(db, abrs) {
+  var z = `INSERT INTO "abbreviations" ("code", "name") VALUES\n`;
+  for(var a of abrs) {
+    z += `('${a.code}', '${a.name}'),\n`;
+    NAME.set(a.code, a.name);
   }
   z = z.substring(0, z.length-2)+`\n`;
   z += `ON CONFLICT ("code") DO NOTHING;`;
@@ -37,12 +36,12 @@ function insert(db, cols) {
 
 function data(db) {
   return new Promise((fres) => {
-    var cols = [];
-    var scol = fs.createReadStream(abbreviations()).pipe(csv.parse({columns: true, comment: '#'}));
-    scol.on('data', (val) => cols.push(val));
-    scol.on('end', () => {
+    var abrs = [];
+    var sabr = fs.createReadStream(abbreviations()).pipe(csv.parse({columns: true, comment: '#'}));
+    sabr.on('data', (val) => abrs.push(val));
+    sabr.on('end', () => {
       createTable(db).then(
-        () => insert(db, cols)).then(
+        () => insert(db, abrs)).then(
         () => createIndex(db)).then(fres);
     });
   });
--- data/index.js
+++ data/index.js
@@ -1,11 +1,13 @@
-const compositions = require('./compositions');
+const abbreviations = require('./abbreviations');
 const columns = require('./columns');
+const compositions = require('./compositions');
 const groups = require('./groups');
 
 function data(db) {
-  return Promise.all([compositions(db), columns(db), groups(db)]);
+  return Promise.all([abbreviations(db), columns(db), compositions(db), groups(db)]);
 };
-data.compositions = compositions;
+data.abbreviations = abbreviations;
 data.columns = columns;
+data.compositions = compositions;
 data.groups = groups;
 module.exports = data;
