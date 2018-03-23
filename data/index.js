d9d5689100ce4bf8a86dc5a5b9e012e0b3aaf056
Subhajit Sahu <wolfram77@gmail.com>
2018-03-24 04:06:07 +0530

    :banana: enable groups table

--- data/index.js
+++ data/index.js
@@ -1,9 +1,11 @@
 const compositions = require('./compositions');
 const columns = require('./columns');
+const groups = require('./groups');
 
 function data(db) {
-  return Promise.all([compositions(db), columns(db)]);
+  return Promise.all([compositions(db), columns(db), groups(db)]);
 };
 data.compositions = compositions;
 data.columns = columns;
+data.groups = groups;
 module.exports = data;
